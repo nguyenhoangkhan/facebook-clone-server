96dc8ec3a59cac6295dc2497620fd156af613365
Khan <nguyenhoangkhan210@gmail.com>
2023-01-04 21:30:38 +0700

    search user api

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,6 +6,7 @@ const userAuth = require("../middlewares/userAuth.js");
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
+router.get("/search", userAuth, userController.searchUser);
 router.get("/:username", userAuth, userController.getProfile);
 
 router.patch("/picture-profile", userAuth, userController.uploadPictureProfile);
